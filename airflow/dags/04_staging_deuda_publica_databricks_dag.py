45424e34fe7ea1548341ddeb4f25ded09a966e70
Samuel Barboza <samu_junior95@hotmail.com>
2026-01-24 09:18:12 -0300

    configuracion del dag que hace la transformacion en staging. Lee raw desde el notebook databricks

--- airflow/dags/04_staging_deuda_publica_databricks_dag.py
+++ airflow/dags/04_staging_deuda_publica_databricks_dag.py
@@ -1,47 +1,36 @@
 from airflow import DAG
-from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator
 from datetime import datetime
 
+# dag transformacion de datos del datalake raw - a staging
 with DAG(
     dag_id="05_staging_deuda_publica_databricks",
     start_date=datetime(2024, 1, 1),
     schedule_interval=None,
     catchup=False,
-    tags=["staging", "databricks"],
-    template_searchpath="/opt/airflow/sql",
+    tags=["staging", "databricks", "azure"],
 ) as dag:
 
-    create_stg_tables = SQLExecuteQueryOperator(
-        task_id="create_stg_tables",
-        conn_id="sqlserver_hacienda",
-        sql="staging/10_create_stg_deuda_publica.sql",
-        hook_params={"schema": "hacienda_dw"},
-    )
-
-    run_databricks = DatabricksSubmitRunOperator(
-    task_id="run_raw_to_stg",
-    databricks_conn_id="databricks_hacienda",
-    json={
-    "run_name": "raw_to_stg_deuda_publica",
-    "tasks": [
-        {
-            "task_key": "transform_raw_to_stg",
-            "warehouse_id": "9f5aee4d22b5dec7",
-            "notebook_task": {
-                "notebook_path": "/Workspace/Users/samubjunior95@gmail.com/01_raw_to_stg_deuda_publica",
-                "base_parameters": {
-                    "execution_date": "{{ ds }}",
-                    "jdbc_host": "sqlserver",
-                    "jdbc_port": "1433",
-                    "jdbc_db": "hacienda_dw",
-                    "jdbc_user": "sa",
-                    "jdbc_pass": "{{ var.value.sqlserver_password }}"
+    run_raw_to_staging = DatabricksSubmitRunOperator(
+        task_id="run_raw_to_staging_parquet",
+        databricks_conn_id="databricks_hacienda",
+        json={
+            "run_name": "raw_to_staging_deuda_publica",
+            "tasks": [
+                {
+                    "task_key": "transform_raw_to_staging",
+                    "existing_cluster_id": "0124-102720-469koryx",
+                    "notebook_task": {
+                        "notebook_path": "/Users/samu_junior95@hotmail.com/01_raw_to_stg_deuda_publica",
+                        "base_parameters": {
+                            "execution_date": "2026-01-22",
+                            "raw_container": "raw",
+                            "staging_container": "staging",
+                            "storage_account": "{{ var.value.azure_storage_account }}",
+                            "dataset": "deuda_publica"
+                        }
+                    }
                 }
-            }
+            ]
         }
-    ]
-}
-)
-
-    create_stg_tables >> run_databricks
+    )
